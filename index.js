ae6879a86ded57a371e7ee2398c0306ba5867672
Earl Harris <earleharrisjr@gmail.com>
2022-11-21 13:19:12 -0500

    create simple react static page with UL about react

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const page = (
+    <div>
+        <img src="react-logo.png" alt="React Logo. Not Tony Soprano" width="50px" ></img>
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>Was released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+            <li>Please contact me if you want to by Season 3 of the Sopranos on DVD</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
