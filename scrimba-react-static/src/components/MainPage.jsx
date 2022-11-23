d56082aa41cb766fde1855cac3d104815bd40e04
Earl Harris <earleharrisjr@gmail.com>
2022-11-23 17:19:49 -0500

    done. add background image

--- scrimba-react-static/src/components/MainPage.jsx
+++ scrimba-react-static/src/components/MainPage.jsx
@@ -7,6 +7,7 @@ export default function MainPage() {
         <ul className="main--facts">
             <li>Was first released in 2013 (6 years after The Sopranos first aired)</li>
             <li>Was orginally created by Jordan Walke</li>
+            <li>Is maintained by Meta</li>
             <li>Has well over 100k stars on GitHub</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
