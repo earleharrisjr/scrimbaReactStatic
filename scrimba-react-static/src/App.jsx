b89fa2b593aa5fddc6fc57a35b75b654fc6f1759
Earl Harris <earleharrisjr@gmail.com>
2022-11-23 16:24:30 -0500

    add nav bar elements and css style. add google font inter

--- scrimba-react-static/src/App.jsx
+++ scrimba-react-static/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Main from './components/Main'
 import NavBar from './components/NavBar'
--- scrimba-react-static/src/components/NavBar.jsx
+++ scrimba-react-static/src/components/NavBar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 
 export default function NavBar() {
-    return <h1>NavBar</h1>
-
+    return (
+        <nav>
+            <img src="src/assets/reactlogo.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
